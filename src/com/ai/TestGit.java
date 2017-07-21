f45419ada1767627534248f8af0f2a4ad3bc5b27
851468365 <851468365@qq.com>
2017-07-21 20:56:10 +0800

    阿斯顿发

--- src/com/ai/TestGit.java
+++ src/com/ai/TestGit.java
@@ -3,7 +3,7 @@ package com.ai;
 public class TestGit {
 	
 	public static void main(String[] args) {
-		System.out.println("hello Git 1234321");
+		System.out.println("hello Git 1234321123");
 	}
 
 }
