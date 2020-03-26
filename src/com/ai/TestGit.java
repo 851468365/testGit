64406fbe0bfc947aa29f5cef99d97af997334f32
自健 <my246294@alibaba-inc.com>
2020-03-26 22:05:35 +0800

    123

--- src/com/ai/TestGit.java
+++ src/com/ai/TestGit.java
@@ -3,8 +3,8 @@ package com.ai;
 public class TestGit {
 
     public static void main(String[] args) {
-        System.out.println("hel123lo java!!");
-        System.out.println(11);
+        System.out.println("hel123345lo java!!");
+        System.out.println(1221);
 
     }
 
